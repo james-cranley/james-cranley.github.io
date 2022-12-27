b084efe007005adbafd3539c62bbe7e72385227e
james-cranley <james.cranley@doctors.org.uk>
2022-12-27 00:31:25 +0000

    cv v2.1

--- cv/start_here_open_with_Rstudio.R
+++ cv/start_here_open_with_Rstudio.R
@@ -6,9 +6,10 @@ library(datadrivencv)
 # run use_datadriven_cv()
 datadrivencv::use_datadriven_cv(full_name = "James Cranley",
 data_location = "https://docs.google.com/spreadsheets/d/1FDKosbW_bC9WvQKVsqhHbHmzXRIXS0sBUvE8cBBl2Ww/edit#gid=917338460",
-pdf_location = "/Users/jc48/Documents/GitHub/james-cranley.github.io/cv/cv.pdf",
-html_location = "/Users/jc48/Documents/GitHub/james-cranley.github.io/cv/cv.html",
-source_location = "/Users/jc48/Documents/GitHub/james-cranley.github.io/cv/")
+pdf_location = "https://james-cranley.github.io/cv/cv.pdf",
+html_location = "https://james-cranley.github.io/cv/cv.html",
+source_location = "/Users/jc48/Documents/GitHub/james-cranley.github.io/cv/",
+use_network_logo = FALSE)
 
 ##### ****** open the cv_printing_functions.R script and edit line 32 to gs4_deauth() ##### ******
 
